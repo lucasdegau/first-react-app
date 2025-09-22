71cfd5334668449fa5bf357b811ec6ffb116138f
Lucas Barbosa Roque <lucas_broque@outlook.com>
2025-09-22 20:39:48 -0300

    favicon update
    updated favicon

--- first-app/src/main.jsx
+++ first-app/src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import favicon from './assets/favicon.png'; 
+
+const setFavicon = () => {
+  let link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+  link.href = favicon;
+};
+
+setFavicon(); //
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
